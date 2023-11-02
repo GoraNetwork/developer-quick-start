8916b249196504632bf9bdba39ed970d372fffae
Tel Ramund <telramundd@gmail.com>
2023-11-03 02:55:06 +0400

    Move demo program execution code to gora library.

--- example_url.py
+++ example_url.py
@@ -48,58 +48,5 @@ def query_oracle_url(request_key: pt.abi.DynamicBytes) -> pt.Expr:
         ),
     )
 
-def demo() -> None:
-
-    # Get Algorand API client instance to talk to local Algorand sandbox node.
-    algod_client = bk.localnet.get_algod_client()
-
-    # Pick a local account to use for asset creation and tests.
-    account = bk.localnet.get_accounts()[0]
-    print("Using local account", account.address)
-
-    # Instantiate ApplicationClient to manage our app.
-    app_client = bk.client.ApplicationClient(
-        client=algod_client,
-        app=example_url_app,
-        signer=account.signer
-    )
-
-    # Deploy our app to the chain.
-    print("Deploying the app...")
-    app_id, app_addr, txid = app_client.create()
-    print("Done, txn ID, app ID, app address:", txid, app_id, app_addr)
-
-    # Supply the app with GORA tokens and ALGO.
-    print("Initializing app for GORA...")
-    app_client.fund(1000000)
-    app_client.call(
-        method=init_gora,
-        token_ref=gora.token_asset_id,
-        main_app_ref=gora.main_app_id,
-    )
-    gora.setup_algo_deposit(algod_client, account, app_addr)
-    gora.setup_token_deposit(algod_client, account, app_addr)
-
-    req_key = uuid.uuid4().bytes;
-    box_name = gora.get_ora_box_name(req_key, app_addr)
-
-    # Reset global storage variable that will be populated by the destination app.
-    pt.App.globalPut(pt.Bytes("oracle_result"), pt.Bytes("")),
-
-    print("Calling the app")
-    result = app_client.call(
-        method=query_oracle_url,
-        request_key=req_key,
-        foreign_apps=[ gora.main_app_id ],
-        boxes=[ (gora.main_app_id, box_name) ],
-    )
-
-    print("Confirmed in round:", result.tx_info["confirmed-round"])
-    print("Txn ID:", result.tx_id)
-
-    inner_logs = [ base64.b64decode(x) for x in result.tx_info["inner-txns"][0]["logs"] ]
-    request_id_msg = next(x for x in inner_logs if re.match(br"^req_id-", x))
-    print("Request ID:", base64.b32encode(request_id_msg[7:]))
-
 if __name__ == "__main__":
-    demo()
+    gora.run_demo_app(example_url_app, query_oracle_url)
--- gora/__init__.py
+++ gora/__init__.py
@@ -6,6 +6,7 @@ import algosdk as asdk
 import hashlib
 import uuid
 import base64
+import re
 
 from typing import Literal as L
 from .inline import InlineAssembly
@@ -341,3 +342,59 @@ def pt_query_classic(request_key, dest_app, dest_method, specs_params,
     ])
 
     return pt.Seq(result)
+
+# Run a demo script.
+def run_demo_app(demo_app, demo_method):
+
+    # Get Algorand API client instance to talk to local Algorand sandbox node.
+    algod_client = bk.localnet.get_algod_client()
+
+    # Pick a local account to use for asset creation and tests.
+    account = bk.localnet.get_accounts()[0]
+    print("Using local account", account.address)
+
+    # Instantiate ApplicationClient to manage our app.
+    app_client = bk.client.ApplicationClient(
+        client=algod_client,
+        app=demo_app,
+        signer=account.signer
+    )
+
+    # Deploy our app to the chain.
+    print("Deploying the app...")
+    app_id, app_addr, txid = app_client.create()
+    print("Done, txn ID:", txid)
+    print("Aapp ID:", app_id)
+    print("App address:", app_addr)
+
+    # Supply the app with GORA tokens and ALGO.
+    print("Initializing app for GORA...")
+    app_client.fund(1000000)
+    app_client.call(
+        method="init_gora",
+        token_ref=token_asset_id,
+        main_app_ref=main_app_id,
+    )
+    setup_algo_deposit(algod_client, account, app_addr)
+    setup_token_deposit(algod_client, account, app_addr)
+
+    req_key = uuid.uuid4().bytes;
+    box_name = get_ora_box_name(req_key, app_addr)
+
+    # Reset global storage variable that will be populated by the destination app.
+    pt.App.globalPut(pt.Bytes("oracle_result"), pt.Bytes("")),
+
+    print("Calling the app")
+    result = app_client.call(
+        method=demo_method,
+        request_key=req_key,
+        foreign_apps=[ main_app_id ],
+        boxes=[ (main_app_id, box_name) ],
+    )
+
+    print("Confirmed in round:", result.tx_info["confirmed-round"])
+    print("Txn ID:", result.tx_id)
+
+    inner_logs = [ base64.b64decode(x) for x in result.tx_info["inner-txns"][0]["logs"] ]
+    request_id_msg = next(x for x in inner_logs if re.match(br"^req_id-", x))
+    print("Request ID:", base64.b32encode(request_id_msg[7:]))
