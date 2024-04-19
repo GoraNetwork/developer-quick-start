0b83a078161b6162ce7df741da80cccb079b733e
Tel Ramund <telramundd@gmail.com>
2024-04-19 21:34:45 +0300

    Add off-chain example Solidity source.

---
+++ evm/example_off_chain.sol
@@ -0,0 +1,68 @@
+pragma solidity >=0.6.12;
+pragma experimental ABIEncoderV2;
+
+// Example smart contract making use of Gora on EVM.
+contract GoraExampleOffChain {
+
+  address goraAddr;
+  bytes wasmBody;
+  bytes public lastValue;
+  bytes32 public lastReqId;
+
+  // Emit debug log message event.
+  function DEBUG(string memory str, uint num, bytes memory bin) internal {
+    emit LogMsg(str, num, bin); // COMMENT OUT to disable debug logging
+  }
+
+  event LogMsg(string message, uint num, bytes bin);
+  event ReceiveResponse(bytes32 reqId, bytes value);
+
+  // Initialize, setting Gora smart contract address that must be passed by
+  // the deploying application.
+  constructor(address initGoraAddr, bytes memory initWasmBody) {
+    goraAddr = initGoraAddr;
+    wasmBody = initWasmBody;
+  }
+
+  // Make a request to Gora using the above configuration.
+  function makeGoraRequest() public {
+
+    bytes[] memory args = new bytes[](1);
+    args[0]= "sm14hp"; // UK postcode of the area queried
+
+    uint16 apiVer = 1;
+    uint16 specType = 0; // 1 - in-place binary WASM
+
+    bytes memory srcArg = abi.encode(apiVer, specType, wasmBody, args);
+    bytes memory reqSig = abi.encodeWithSignature(
+      "request(uint8,bytes,bytes,address,string,bytes)", 3,
+      "gora://offchain/inline", srcArg, address(this), "receiveGoraResponse", ""
+    );
+
+    (bool isOk, bytes memory res) = goraAddr.call(reqSig);
+    if (!isOk)
+      revert("Unable to make Gora request");
+
+    DEBUG("Gora request made", block.number, res);
+  }
+
+  function bytes32ToBytes(bytes32 src) internal pure returns (bytes memory) {
+    bytes memory res = new bytes(32);
+    assembly { mstore(add(res, 32), src) }
+    return res;
+  }
+
+  // Receive a response from Gora to the above request.
+  function receiveGoraResponse(bytes32 reqId, uint256 reqRound, address requester,
+                               bytes calldata value, bytes calldata userData,
+                               uint256 srcErrors) public {
+
+    require(msg.sender == goraAddr, "sender is not the Gora main contract");
+    DEBUG("Gora response received", 0, bytes32ToBytes(reqId));
+
+    // Save received variables for further inspection.
+    lastValue = value;
+    lastReqId = reqId;
+    emit ReceiveResponse(reqId, value);
+ }
+}
