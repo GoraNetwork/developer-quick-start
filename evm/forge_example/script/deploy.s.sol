fae06f562eb6c1069ba8259ac80edb30bd03d212
KaiReytsu <kai.reytsu@gmail.com>
2024-10-17 15:04:46 +0300

    Delete comments

--- evm/forge_example/script/deploy.s.sol
+++ evm/forge_example/script/deploy.s.sol
@@ -6,22 +6,16 @@ import "../src/example_basic.sol";
 
 contract DeployGoraExample is Script {
     function run() external returns (address) {
-        // Загрузка приватного ключа из переменной среды
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         
-        // Загрузка адреса Gora из переменной среды
         address goraAddress = vm.envAddress("GORA_ADDRESS");
 
-        // Начало широковещательной транзакции
         vm.startBroadcast(deployerPrivateKey);
 
-        // Создание и развертывание контракта GoraExample
         GoraExample goraExample = new GoraExample(goraAddress);
 
-        // Завершение широковещательной транзакции
         vm.stopBroadcast();
 
-        // Возвращаем адрес развернутого контракта
         console.log("GoraExample deployed at:", address(goraExample));
         return address(goraExample);
     }
