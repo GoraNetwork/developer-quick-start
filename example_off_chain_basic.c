a5925546d96d0bc7f3442c038c825306f23ede5b
Tel Ramund <telramundd@gmail.com>
2024-11-15 08:08:36 +0300

    Update basic off-chain example code.

--- example_off_chain_basic.c
+++ example_off_chain_basic.c
@@ -1,6 +1,9 @@
+// This is minimal example program demonstrating the use of Gora off-chain
+// computation API. It returns string "Hello Gora!" as the oracle value.
+
 #include "gora_off_chain.h"
 
-static char* basic_result = "Hello world!";
+static char* basic_result = "Hello Gora!";
 
 GORA_DECLARE_FUNC_MAIN
 int gora_main(struct gora_context_t* gora_ctx) {
